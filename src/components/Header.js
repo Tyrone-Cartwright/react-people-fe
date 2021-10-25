fd6243c44a5853a325c118e6508e1afb474db0da
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2021-10-25 14:46:45 -0400

    wip: added a nav link to header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
-const Header = () => {
-  return <div>Header</div>;
+import { Link } from 'react-router-dom';
+
+const Header = (props) => {
+  return (
+    <header className='Header'>
+      <Link to='/'>
+        <div>People App</div>
+      </Link>
+    </header>
+  );
 };
 
 export default Header;

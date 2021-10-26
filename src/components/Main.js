6ce7d2b2442f47946834ec69d1478cff80a16ed5
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2021-10-26 10:31:06 -0400

    wip: update and delete complete

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,57 @@
 import { useState, useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Index from '../pages/Index';
 import Show from '../pages/Show';
 
 const Main = (props) => {
-  // Declaring state
-  const [people, setPeople] = useState(null);
-  // URL
-  const URL = 'https://people-backend-react.herokuapp.com/people';
+  const [people, setPeople] = useState([]); // NOTE: normally I would set this to an empty array
 
-  // Get People
+  // const URL = 'https://people-backend-react.herokuapp.com/people';
+  const URL = 'http://localhost:3001/people/'; // Production
+
+  // helper functions for getting and creating people
   const getPeople = async () => {
-    const data = await fetch(URL).then((res) => res.json());
-    // const response = await fetch(URL);
-    // const data = await response.json();
+    // alternative syntax for resolving all promises
+    // const data = await fetch(BASE_URL).then(response => response.json());
+    const response = await fetch(URL);
+    const data = await response.json();
     setPeople(data);
   };
 
   const createPeople = async (person) => {
-    // make post request to create People
     await fetch(URL, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-type': 'Application/json',
       },
       body: JSON.stringify(person),
     });
-    // update list of People
+
+    getPeople(); // get people and update state after creating a person
+  };
+
+  const updatePeople = async (person, id) => {
+    await fetch(URL + id, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'Application/json',
+      },
+      body: JSON.stringify(person),
+    });
+    getPeople(); // why should we call getPeople() ?
+  };
+
+  const deletePeople = async (id) => {
+    await fetch(URL + id, { method: 'DELETE' });
     getPeople();
   };
 
-  useEffect(() => getPeople(), []);
+  // make sure we get people when the application loads
+  // in other words, we need a side effect to occur as a result of the page loading
+  // we will use the useEffect hook to have it's effect function run on page load
+
+  // useEffect(() => getPeople()) // why does this syntax create an infinite loop
+  useEffect(() => getPeople(), []); // run once on page load, but not for any subsequent state changes
 
   return (
     <main>
@@ -38,7 +59,23 @@ const Main = (props) => {
         <Route exact path='/'>
           <Index people={people} createPeople={createPeople} />
         </Route>
-        <Route path='/people/:id' render={(rp) => <Show {...rp} />} />
+        <Route
+          path='/people/:id'
+          render={(rp) =>
+            // render props or "rp" for short
+            // includes three objects => Location, Match, History
+            people.length ? (
+              <Show
+                {...rp}
+                people={people}
+                updatePeople={updatePeople}
+                deletePeople={deletePeople}
+              />
+            ) : (
+              <Redirect to='/' />
+            )
+          }
+        />
       </Switch>
     </main>
   );
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,70 @@
-const Show = () => {
-  return <div>Show</div>;
+import { useState } from 'react';
+
+const Show = (props) => {
+  const id = props.match.params.id;
+  const people = props.people;
+  const person = people.find((p) => p._id === id);
+
+  // state for form
+  const [editForm, setEditForm] = useState(person);
+
+  // handleChange function for form
+  const handleChange = (evt) => {
+    setEditForm((prevState) => ({
+      ...prevState,
+      [evt.target.name]: evt.target.value,
+    }));
+  };
+
+  // handleSubmit for form
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    props.updatePeople(editForm, person._id);
+    // redirect people back to index page
+    props.history.push('/'); // allows us to push locations into the browser's history
+    // programmatically route us back to the homepage
+  };
+
+  // delete function to remove a Person
+  const removePerson = (evt) => {
+    props.deletePeople(person._id);
+    props.history.push('/');
+  };
+
+  return (
+    <div className='person'>
+      <h1>{person.name}</h1>
+      <h2>{person.title}</h2>
+      <img src={person.image} alt={person.name} />
+      <button id='delete' onClick={removePerson}>
+        DELETE
+      </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={editForm.name}
+          name='name'
+          placeholder='name'
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          value={editForm.image}
+          name='image'
+          placeholder='image URL'
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          value={editForm.title}
+          name='title'
+          placeholder='title'
+          onChange={handleChange}
+        />
+        <input type='submit' value='Update Person' />
+      </form>
+    </div>
+  );
 };
 
 export default Show;
